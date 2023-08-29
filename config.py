3fcd1498058c439b6dbea890ebbd5eeb58d30208
HarmenHii <143473558+HarmenHii@users.noreply.github.com>
2023-08-29 11:41:37 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cvviebwbnjdb.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "harmenhii-employee"
+customregion = "us-east-1"
+
